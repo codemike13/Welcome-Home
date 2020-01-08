d7c8dab161c568e0d6b09b934592d26d9c703f9c
Anthony Michael Stephens <mike.stephens@codeimmersives.com>
2020-01-08 14:22:02 -0500

    Creating basic shell starting with NAv bar

---
+++ welcome-home-front/src/components/Nav/Nav.js
@@ -0,0 +1,132 @@
+import React, { Component } from 'react'
+import { apiAuth, apiHandleSignUpAndLogIn } from '../../api/api'
+import setAuthJWT from '../../api/setAuthJWT'
+
+class Nav extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: '',
+            password: '',
+            isAuth: false,
+            loggedInEmail: '',
+            errorMessage: false,
+            errorToggle: false
+        }
+    }
+
+    componentDidMount = () => {
+        apiAuth()
+            .then(userObj => {
+                this.setState({
+                    isAuth: true,
+                    loggedInEmail: userObj.email
+                }, () => {
+                    this.props.appHandleAuthSubmit()
+                })
+            })
+            .catch(err => console.log(err))
+    }
+
+    handleInputOnChange = (event) => {
+        this.setState({
+            [event.target.name] : event.target.value
+        })
+    }
+
+    handleInputOnSubmit = (event) => {
+        event.preventDefault()
+
+        apiHandleSignUpAndLogIn({
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(result => {
+                const { email } = result
+
+                this.setState({
+                    isAuth: true,
+                    loggedInEmail: email,
+                    email: '',
+                    password: '',
+                    errorToggle: false,
+                    errorMessage: false
+                }, () => {
+                    this.props.appHandleAuthSubmit()
+                })
+            })
+            .catch(errorMessage => {
+                this.setState({
+                    errorToggle: true,
+                    errorMessage: errorMessage
+                })
+            })
+    }
+
+    logOut = () => {
+        this.setState({
+            isAuth: false
+        }, () => {
+            this.props.appHandleLogout()
+
+            localStorage.removeItem('jwtToken')
+
+            setAuthJWT(null)
+        })
+    }
+
+    render() {
+        return (
+            <>
+            <nav className='navbar navbar-light bg-light'>
+                <a className='navbar-brand' href='\'>CODE IMMERSIVES</a>
+                { this.state.isAuth ? (
+                    <>
+                        <span>
+                            { this.state.loggedInEmail }
+                        </span>
+                        <button 
+                            className='btn btn-warning'
+                            onClick={ this.logOut }
+                        >
+                            Log out
+                        </button>
+                    </>
+                ) : (
+                    <form 
+                    className='form-inline'
+                    onSubmit={ this.handleInputOnSubmit }
+                    >
+                        <input 
+                            type='text' 
+                            placeholder='email'    
+                            className='form-control mr-sm-2'
+                            name='email'
+                            onChange={ this.handleInputOnChange }
+                        />
+                        <input 
+                            type='text' 
+                            placeholder='password' 
+                            className='form-control mr-sm-2'
+                            name='password'
+                            onChange={ this.handleInputOnChange }
+                        />
+                        <button className='btn btn-outline-success my-2 my-sn-0'>Sign Up | Sign In</button>
+                    </form>
+                ) }
+            </nav>
+            <span
+                style={{ padding: '0px' }}
+                className={ this.state.errorToggle ? 'alert alert-danger' : '' }
+            >
+                { this.state.errorToggle ? this.state.errorMessage : '' }
+            </span>
+            </>
+        )
+
+        
+    }
+}
+export default Nav
+
---
+++ welcome-home-front/src/components/TodoList/TodoList.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import './TodoList.css'
+import Todo from './Todo/Todo'
+import PropTypes from 'prop-types'
+
+export default class TodoList extends Component {
+    state = {
+        newTodo: ''
+    }
+
+    handleNewTodoSubmit = (event) => {
+        event.preventDefault()
+
+        this.props.appHandleAddNewTodoList(this.state)
+
+        this.setState({
+            newTodo: ''
+        })
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            newTodo: event.target.value
+        })
+    }
+
+    showTodoList() {
+        console.log(`this.props.todoList: `, this.props.todoList);
+
+        return this.props.todoList.map((item) => {
+            return (
+                <Todo
+                    key={item._id}
+                    id={item._id}
+                    item={item.todo}
+                    completed={item.completed}
+                    todoHandleNewEditTodoByID={this.props.appHandleNewEditTodoByID}
+                    todoHandleDeleteByID={this.props.appHandleDeleteByID}
+                    todoHandleCompleteByID={this.props.appHandleCompleteByID}
+                />
+            )
+        })
+    }
+
+    render() {
+        return (
+            <>
+                <form
+                    onSubmit={this.handleNewTodoSubmit}
+                >
+                    <input
+                        name='newTodo'
+                        value={this.state.newTodo}
+                        onChange={this.handleOnChange}
+                    />
+                    <button>Submit</button>
+                </form>
+                <ul style={styles.listStyle}>
+                    {
+                        this.props.todoList ? this.showTodoList() : null
+                    }
+                </ul>
+            </>
+        )
+    }
+}
+TodoList.propTypes = {
+    todolist: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string,
+            todo: PropTypes.string,
+            completed: PropTypes.bool
+        })
+    )
+}
+
+const styles = {
+    listStyle: {
+        listStyleType: 'none'
+    }
+}
