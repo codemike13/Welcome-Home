898020082c20543d53b846df832356bc5e00fdb1
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-01-08 14:59:15 -0500

    Added passport module framework for authentication

--- welcome-home-back/app.js
+++ welcome-home-back/app.js
@@ -31,6 +31,10 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(passport.initialize())
+
+require('./passport/passport')(passport)
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
