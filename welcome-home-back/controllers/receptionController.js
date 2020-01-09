4a25e85774a686daf5b051e52e0cf12ffb96f5ba
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-01-09 11:01:14 -0500

    Controller Framework Created receptionController and superUserController files with skeleton functions to be coded later

---
+++ welcome-home-back/controllers/receptionController.js
@@ -0,0 +1,35 @@
+const Resident = require('../models/Resident')
+
+module.exports = {
+    createCalendarEvent: (params) => {
+
+    },
+
+    updateCalendarEvent: (params) => {
+
+    },
+
+    deleteCalendarEvent: (params) => {
+
+    },
+
+    packageAlert: (params) => {
+
+    },
+
+    closeOutPackage: (params) => {
+
+    },
+
+    amenityReservation: (parmas) => {
+
+    },
+
+    amenityUpdate: (params) => {
+        
+    },
+
+    amenityDelete: (params) => {
+        
+    }
+}
---
+++ welcome-home-back/controllers/superUserController.js
@@ -0,0 +1,15 @@
+const Reception = require('../models/Reception')
+
+module.exports = {
+    newReception: (params) => {
+
+    },
+
+    updateReception: (parmas) => {
+
+    },
+
+    deleteReception: (params) => {
+
+    }
+}
