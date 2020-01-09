c0e857bc97f69fa30b22c2f937af0f135b71d871
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-01-09 12:16:57 -0500

    Placeholder Footer Finished creating a placeholder footer component for main display

--- welcome-home-front/src/components/footer/Footer.js
+++ welcome-home-front/src/components/footer/Footer.js
@@ -1,17 +1,19 @@
+import React from 'react'
+
 const Footer = () => {
     return (
         <>
         <div id="main" style={styles.main}>
             <div id="main-left" style={styles.mainLeft}>
-
+                (Copyright placeholder)
             </div>
 
             <div id="main-center" style={styles.mainCenter}>
-
+                (Contact Information)
             </div>
 
             <div id="main-right" style={styles.mainRight}>
-
+                (Other Links)
             </div>
 
         </div>
@@ -22,7 +24,7 @@ const Footer = () => {
 const styles = {
     main: {
         width: '100vw',
-        height: '15vh',
+        height: '10vh',
         backgroundColor: 'gray',
         display: 'grid',
         gridTemplateColumns: '1fr 1fr 1fr',
@@ -31,17 +33,23 @@ const styles = {
     mainLeft: {
         display: 'flex',
         flexDirection: 'column',
-        gridArea: '1 / 1 / span 1 / span 1'
+        gridArea: '1 / 1 / span 1 / span 1',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     mainCenter: {
         display: 'flex',
         flexDirection: 'column',
-        gridArea: '1 / 2 / span 1 / span 1'
+        gridArea: '1 / 2 / span 1 / span 1',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     mainRight: {
         display: 'flex',
         flexDirection: 'column',
-        gridArea: '1 / 3 / span 1 / span 1'
+        gridArea: '1 / 3 / span 1 / span 1',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 }
 
