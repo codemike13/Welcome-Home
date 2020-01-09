9e245c224c921fe74e0ea7bd1ba8155f352d289e
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-01-09 13:21:35 -0500

    Passport Login Options Created passport logic flow for when a user logs in. First checks if they are a resident, then a receptionist, then superUser. If none found, returns false

--- welcome-home-back/passport/passport.js
+++ welcome-home-back/passport/passport.js
@@ -1,6 +1,8 @@
 const Jwtstrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const User = require('../models/Resident')
+const Resident = require('../models/Resident')
+const Reception = require('../models/Reception')
+const Admin = require('../models/Admin')
 
 const key = process.env.SECRET_KEY || 'defaultSecretKey'
 
@@ -10,10 +12,22 @@ opts.secretOrKey = key
 
 module.exports = passport => {
     passport.use(new Jwtstrategy(opts, (jwt_payload, done) => {
-        User.findById(jwt_payload.id)
-            .then(user => {
-                if (user) {
+        Resident.findById(jwt_payload.id)
+            .then(resident => {
+                if (resident) {
                     return done(null, user)
+                } else if (!resident) {
+                    Reception.findById(jwt.passport.id)
+                        .then(reception => {
+                            return done(null, reception)
+                        })
+                        .catch(err => console.log(err))
+                } else if (!resident && !reception) {
+                    Admin.findById(jwt_payload.id)
+                        .then(admin => {
+                            return done(null, admin)
+                        })
+                        .catch(err => console.log(err))
                 } else {
                     return done(null, false)
                 }
--- welcome-home-front/src/components/footer/Footer.js
+++ welcome-home-front/src/components/footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
             </div>
 
             <div id="main-center" style={styles.mainCenter}>
-                (Contact Information)
+                <a href="mailto:mike.stephens@codeimmersives.com,chris.brindle@codeimmersives.com?cc=micp1084@aol.com&subject=Welcome Home Contact">Contact</a>&nbsp;site administrators
             </div>
 
             <div id="main-right" style={styles.mainRight}>
@@ -42,7 +42,6 @@ const styles = {
     },
     mainCenter: {
         display: 'flex',
-        flexDirection: 'column',
         gridArea: '1 / 2 / span 1 / span 1',
         justifyContent: 'center',
         alignItems: 'center'
