9273dbd89fde87a11b1aa3d2c688d741136c271c
Christopher Brindle <chris.brindle@codeimmersives.com>
2020-01-11 16:50:36 -0500

    Reception Buttons Added event listeners to add resident, update resident, and delete resident button on reception manage component. Each button now loads in its corresponding component

---
+++ welcome-home-front/src/components/Manage/NewResident.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewResident = () => {
+    return(
+        <>
+        <h1>New User Form</h1>
+        </>
+    )
+}
+
+export default NewResident
--- welcome-home-front/src/components/Manage/manage.js
+++ welcome-home-front/src/components/Manage/manage.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react"
 import background from '../../images/bg-1.jpg'
+import NewResident from './NewResident'
+import UpdateResident from './UpdateResident'
+import DeleteResident from './DeleteResident'
 
 class Manage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            user: 'reception'
+            user: 'reception',
+            newUser: false,
+            updateUser: false,
+            deleteUser: false
         }
+    }
+
+    showNewResident = () => {
+        this.setState({
+            newUser: !this.state.newUser,
+            updateUser: false,
+            deleteUser: false
+        })
+    }
+
+    showUpdateResident = () => {
+        this.setState({
+            newUser: false,
+            updateUser: !this.state.updateUser,
+            deleteUser: false
+        })
+    }
+
+    showDeleteResident = () => {
+        this.setState({
+            newUser: false,
+            updateUser: false,
+            deleteUser: !this.state.deleteUser
+        })
     }
 
     render() {
@@ -14,34 +44,54 @@ class Manage extends Component {
             
             <>
             <div id="app">
+
+                {/* Resident View */}
                 {this.state.user === 'resident' ?
                 'resident view'
                 :
                 ''
                 }
 
+                {/* Reception View */}
                 {this.state.user === 'reception' ?
                 <div id="reception-app" style={styles.reception.app}>
                     
                     {/* Left side of page */}
                     <div id="reception-title" style={styles.reception.title}>
-                        <h1>Reception Main Menu</h1>
+                        <h2>Reception Management Menu</h2>
                     </div>
                     <div id="reception-menu" style={styles.reception.menu}>
-                        <button className="btn btn-success" style={styles.reception.menuButtons}>Add New Resident</button>
-                        <button className="btn btn-success" style={styles.reception.menuButtons}>Update Resident's Information</button>
-                        <button className="btn btn-danger" style={styles.reception.menuButtons}>Delete Resident</button>
+                        <button onClick={this.showNewResident} className="btn btn-success" style={styles.reception.menuButtons}>Add New Resident</button>
+                        <button onClick={this.showUpdateResident} className="btn btn-success" style={styles.reception.menuButtons}>Update Resident's Information</button>
+                        <button onClick={this.showDeleteResident} className="btn btn-danger" style={styles.reception.menuButtons}>Delete Resident</button>
                     </div>
                     
                     {/* Right side of page */}
                     <div id="reception-content" style={styles.reception.content}>
-                        <p>CONTENT LOADED HERE... REACT HOOOOOAH!</p>
+                        {this.state.newUser ? 
+                        <NewResident />
+                        :
+                        ''
+                        }
+
+                        {this.state.updateUser ?
+                        <UpdateResident />
+                        :
+                        ''
+                        }
+
+                        {this.state.deleteUser ?
+                        <DeleteResident />
+                        :
+                        ''
+                        }
                     </div>
                 </div>
                 :
                 ''
                 }
 
+                {/* Admin View */}
                 {this.state.user === 'admin' ?
                 <p>admin view</p>
                 :
@@ -61,9 +111,8 @@ const styles = {
 
     reception: {
         app: {
-            overflowY: 'scroll',
             display: 'grid',
-            gridTemplateColumns: '50% 50%',
+            gridTemplateColumns: '40% 60%',
             gridTemplateRows: '20% 80%',
             padding: '3vh 0 0 0 ',
             backgroundImage: `url(${background})`,
@@ -86,13 +135,15 @@ const styles = {
             gridArea: '2 / 1 / span 1 / span 1'
         },
         menuButtons: {
-            margin: '1vh 0 1vh 0'
+            margin: '1vh 0 1vh 0',
+            border: '1px solid black'
         },
         content: {
             display: 'flex',
             gridArea: '1 / 2 / span 2 / span 1',
             alignItems: 'center',
-            justifyContent: 'center'
+            justifyContent: 'center',
+            overflowY: 'scroll'
         }
     },
 
